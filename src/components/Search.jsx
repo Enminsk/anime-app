becdf1f15bdc67a7f8b1ba1785181fac6105f27c
Enminsk <enminsk@yandex.ru>
2022-05-24 20:29:13 +0300

    add(router): add router

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,10 @@
 import { useCallback, useEffect, useState } from 'react';
-/* import { useNavigate } from "react-router-dom"; */
+import { useNavigate } from "react-router-dom";
 import { debounce } from 'lodash';
+import { Route } from 'react-router';
 
 import { getSearch } from '../api';
+import css from './search.styles.module.css';
 
 
 export function Search() {
@@ -13,30 +15,32 @@ export function Search() {
         setValue(event.target.value)
     };
 
-    const getSearchDebounce = useCallback(debounce(getSearch, 1500));
-/*     const navigate = useNavigate(); */
+    const getSearchDebounce = useCallback(debounce((value) => getSearch(value).then(({ data }) => {
+        setOptions(data)
+    }), 1500), []);
+    const navigate = useNavigate();
 
     useEffect(() => {
         if (value.length > 2) {
-            getSearchDebounce(value).then(({ data }) => {
-                setOptions(data)
-            });
+            getSearchDebounce(value)
         }
     }, [value]);
 
-/*     const onOptionClick = (o) => {
+    const onOptionClick = (o) => {
         navigate(`/amime/${o.mal_id}`)
-    } */
+    }
 
     return (
         <div>
             <input value={value} onChange={inputChangeHandler}></input>
-            <ul>
-                {options.map((option) => (
-                    <li key={option.mal_id}>
-                        {option.title}
-                    </li>
-                ))}
+            <ul className={css.input}>
+                <Route>
+                    {options.map((option) => (
+                        <li key={option.mal_id} onClick={onOptionClick}>
+                            {option.title}
+                        </li>
+                    ))}
+                </Route>
             </ul>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Router } from 'react-router';
 //import { Provider } from 'react-redux';
 import App from './components';
 import { Search } from './components';
@@ -9,12 +10,11 @@ import { Search } from './components';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 /*     <Provider store = {store}> */
-    <>
+    <Router>
         <Search />
         <App type="anime" />
         <App type="manga" />
-{/*         <App type="people" /> */}
-    </>
+    </Router>
 /*     </Provider> */
 );
 
